c9ad4c6dcaac5e01c2786b7f03faa10585001a4f
karenhernandez <khernandez0329@gmail.com>
2023-12-17 20:13:59 -0800

    completed

--- testing/dog_property_test.py
+++ testing/dog_property_test.py
@@ -3,10 +3,10 @@ import pytest
 
 
 class TestDogProperties:
-    '''Class Dog in dog.py'''
+    """Class Dog in dog.py"""
 
     def test_name_breed_valid(self):
-        '''validates name and breed properties are initialized with valid values'''
+        """validates name and breed properties are initialized with valid values"""
         try:
             # No exception should be thrown since name and breed are valid
             dog = Dog("Fido", "Corgi")
@@ -15,17 +15,17 @@ class TestDogProperties:
             assert False, f'Dog("Fido", "Corgi") raised an exception {exc}'
 
     def test_name_is_string_valid_length(self):
-        '''validates name property is assigned a string between 1 and 25 characters'''
+        """validates name property is assigned a string between 1 and 25 characters"""
         dog = Dog("Fido", "Corgi")
         with pytest.raises(ValueError):
             dog.name = 7  # not a string
         with pytest.raises(ValueError):
-            dog.name = ''  # too short
+            dog.name = ""  # too short
         with pytest.raises(ValueError):
-            dog.name = 'Fido the adorable Corgi who likes to steal socks'  # too long
+            dog.name = "Fido the adorable Corgi who likes to steal socks"  # too long
 
     def test_breed_is_approved_breed(self):
-        '''validates breed property is in list of approved choices'''
+        """validates breed property is in list of approved choices"""
         dog = Dog("Snoopy", "Beagle")
         with pytest.raises(ValueError):
             dog.breed = "Poodle"  # not an approved breed
